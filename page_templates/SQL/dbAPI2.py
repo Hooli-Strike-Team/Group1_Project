778fa2b45ebbf3a10d53d68aa647b37c619e2e76
Matthew Tuttle <matu8568@colorado.edu>
2023-04-25 16:44:25 +0000

    Created copy of dbAPI , now works

---
+++ page_templates/SQL/dbAPI2.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3 
+
+#Authors: Paul Schneider TEST
+#Date: April 2023
+#Purpose: Create a database for the website 
+
+import sqlite3 
+           
+def create(db_filename):
+    conn = sqlite3.connect(db_filename) 
+    c = conn.cursor() 
+    
+    #Create User_Account Table
+    c.execute("""
+            CREATE TABLE IF NOT EXISTS User_Account(
+                Username VARCHAR(32),
+                Password VARCHAR(16),
+                First_Name VARCHAR(50),
+                Last_Name VARCHAR(50),
+                Email VARCHAR(320), 
+                PRIMARY KEY(Username))""")
+    
+    #Create User_Achievements Table
+    c.execute("""
+    
+            CREATE TABLE IF NOT EXISTS User_Achievements(
+                Username VARCHAR(32),
+                Inquisitor BOOLEAN,
+                LoneWolf BOOLEAN,
+                PuzzleMaster BOOLEAN,
+                RiskTaker BOOLEAN,
+                SpeedRunner BOOLEAN,
+                Conqueror BOOLEAN,
+                PRIMARY KEY(Username), 
+                FOREIGN KEY(Username) REFERENCES User_Account(Username)) 
+                
+            """)
+    
+    #Create Achievement_Stats Table 
+    c.execute("""
+            
+            CREATE TABLE IF NOT EXISTS Achievement_Stats(
+                Username VARCHAR(32),
+                EasyGamesCompleted INT,
+                MedGamesCompleted INT,
+                HardGamesCompleted INT,
+                Best_Time_Easy FLOAT,
+                Best_Time_Med FLOAT,
+                Best_Time_Hard FLOAT,
+                AccountLevel FLOAT,
+                PRIMARY KEY(Username),
+                FOREIGN KEY(Username) REFERENCES User_Account(Username)) 
+                
+            """)
+    
+    #Create Games_In_Progress Table
+    c.execute("""
+    
+            CREATE TABLE IF NOT EXISTS Games_In_Progress(
+                Username VARCHAR(32),
+                Game_ID INT,
+                Current_Time TEXT,
+                Game BLOB,
+                Difficulty VARCHAR(6),
+                PRIMARY KEY(Game_ID), 
+                FOREIGN KEY(Username) REFERENCES User_Account(Username)) 
+
+            """)
+    
+    #Create Game_Settings Table 
+    c.execute("""
+            
+            CREATE TABLE IF NOT EXISTS Game_Settings(
+                Username INT,
+                Show_Clock BOOLEAN,
+                Show_Mistakes_Counter BOOLEAN,
+                PRIMARY KEY(Username), 
+                FOREIGN KEY(Username) REFERENCES Games_In_Progress(Username)) 
+            
+            """)
+    
+    conn.commit()
+    conn.close()
+    
+                
+def print_tables(db_filename): # Prints information in tables to be used for debugging 
+    conn = sqlite3.connect(db_filename)
+    c = conn.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    
+    print("\nTables:")
+    for t in c.fetchall() :
+        print("\t[%s]"%t[0])
+        
+        print ("\tColumns of", t[0])
+        c.execute("PRAGMA table_info(%s);"%t[0])
+        for attr in c.fetchall() :
+            print ("\t\t", attr)
+
+            
+def delete_tables(db_filename):
+    conn = sqlite3.connect(db_filename) 
+    with sqlite3.connect(db_filename) as conn:
+        c = conn.cursor() 
+        #Create User_Account Table
+        c.execute("DROP TABLE IF EXISTS User_Account;")
+        c.execute("DROP TABLE IF EXISTS User_Achievements;")
+        c.execute("DROP TABLE IF EXISTS Achievement_Stats;")
+        c.execute("DROP TABLE IF EXISTS Games_In_Progress;")
+        c.execute("DROP TABLE IF EXISTS Game_Settings;")
+        conn.commit()
+        
